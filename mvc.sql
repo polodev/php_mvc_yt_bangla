8da66afa3f70b8a6b5bb05495240ebef19c486fb
polo dev <polodev10@gmail.com>
2018-01-24 02:13:53 +0600

    added readme

---
+++ mvc.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.16  Distrib 10.1.29-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: college
+-- ------------------------------------------------------
+-- Server version	10.1.29-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `teachers`
+--
+
+DROP TABLE IF EXISTS `teachers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teachers`
+--
+
+LOCK TABLES `teachers` WRITE;
+/*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
+INSERT INTO `teachers` VALUES (1,'farhad','farhad@gmail.com'),(2,'sumon','sumon@gmail.com'),(3,'sarwar','sarwar@gmail.com'),(4,'arafat','arafat@gmail.com'),(5,'tasnia','tasnia@gmail.com'),(6,'mimi','mimi@gmail.com'),(7,'tanim','tanim@gmail.com');
+/*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-24  2:07:00
---
+++ readme.md
@@ -0,0 +1,24 @@
+# mvc in php
+~~~bash
+R
+C <======>  M
+V
+~~~
+
+mvc just follow division of the work paradigm. We will split our code in 3 different area. Mvc is very popular software paradigm. Most of php framework like  Laravel, codeignite, yii, cake php, symfony following mvc paradigm. Its very simple to understand. 
+
+## view
+`view` will show view part like html, iterating data and view for end user.
+
+## controller
+Controller do logic part and getting data from model and show view page     
+
+## model   
+Model is a data layer. It's responsible for managing data
+
+
+## Routing   
+Route will determine which controller will be loaded based on url route.   
+
+
+Thanks
